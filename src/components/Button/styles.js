3a8d2b71476727f182d58816b31c06c6c17fe314
AlexandreRCP <alex.rcp@hotmail.com>
2022-11-24 13:03:35 -0300

    Inicio projeto Wiki GitHub

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.div`
+    
+    background-color: #FAFAFA;
+    border-color: 1px solid #FAFAFA;
+    border-radius: 20px;
+    align-items: center;
+
+    height: 62px;
+    width: 80%;
+
+    margin: 20px;
+
+    &:hover {
+        background-color: #FAFAFA60;
+        cursor: pointer;
+    }
+`
---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h5 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottum: 20px;
+    }
+
+    a {
+        color: #00fbff;
+        margin-top: 15px;
+    }
+
+    a.remover {
+        color: #FF0000;
+        margin-top: 15px;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 15px 0;
+    }
+`
